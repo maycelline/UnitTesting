d1c2c80aeaaa5eca2f8af2e450f18a5861db4655
KaisarV <kaisar.valentino123@gmail.com>
2022-10-19 22:08:05 +0700

    test email

--- src/test/java/EmailTest.java
+++ src/test/java/EmailTest.java
@@ -6,12 +6,31 @@ public class EmailTest {
     Controllers cr = new Controllers();
 
     @Test
-    public void testEmailUsingAt(){
+    public void testEmailNotUsingAt(){
         String email = "randomwordatgmailcom";
 
         boolean resultExpected=false;
         boolean actual = cr.validateEmail(email);
         Assertions.assertEquals(resultExpected, actual);
     }
+
+    @Test
+    public void testEmailUsingAt(){
+        String email = "kaisar.valentino123@gmail.com";
+
+        boolean resultExpected=true;
+        boolean actual = cr.validateEmail(email);
+        Assertions.assertEquals(resultExpected, actual);
+    }
+
+    @Test
+    public void testEmailIsNull(){
+        String email = "";
+
+        boolean resultExpected=false;
+        boolean actual = cr.validateEmail(email);
+        Assertions.assertEquals(resultExpected, actual);
+    }
+
 
 }
