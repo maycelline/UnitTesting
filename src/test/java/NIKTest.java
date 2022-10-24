3490c119c61edabd8dd6902b9edb0ffdbc97789d
Maycelline S <maycellineselvyanti@gmail.com>
2022-10-25 05:32:14 +0700

    tambah test case buat NIK

--- src/test/java/NIKTest.java
+++ src/test/java/NIKTest.java
@@ -15,7 +15,7 @@ public class NIKTest {
     }
 
     @Test
-    public void testValidateNIK_is16chars() {
+    public void testValidateNIK_is16chars_andValid() {
         String NIK = "3204285502020017";
         boolean expected = true;
 
@@ -24,7 +24,16 @@ public class NIKTest {
     }
 
     @Test
-    public void testValidateNIK_moreThan16Chars(){
+    public void testValidateNIK_is16chars_andInvalid() {
+        String NIK = "3204285502020a17";
+        boolean expected = false;
+
+        boolean actual = cr.validateNIK(NIK);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testValidateNIK_moreThan16Chars() {
         String NIK = "319894798178974381784371";
         boolean expected = false;
 
@@ -33,7 +42,7 @@ public class NIKTest {
     }
 
     @Test
-    public void testValidateNIK_withAlphabet(){
+    public void testValidateNIK_withAlphabet() {
         String NIK = "a763716672";
         boolean expected = false;
 
